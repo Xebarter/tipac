03460ee32469b99430573414c521a27c3843c0ca
Xebarter <sebenock027@gmail.com>
2025-04-26 16:18:44 +0300

    added favicon

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourdomain.com', // replace with your domain
+    generateRobotsTxt: true, // also creates a robots.txt file
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientBody from "./ClientBody";
+<meta name="apple-mobile-web-app-title" content="tipac.co.ug" />
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
